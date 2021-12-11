cc2da613876146cf00fdd0bbab3ecc7197ae52ae
dprincecoder <dprincecoder@gmail.com>
2021-12-11 13:42:09 +0100

    chat update

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,33 +1,64 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./chat.scss";
 import Avatar from "@material-ui/core/Avatar";
-import { useHistory } from "react-router-dom";
 import InputForm from "../../components/forms/inputs/InputForm";
 import Button from "../../components/forms/button/Button";
+import { Link, useHistory, useParams } from "react-router-dom";
+import DB from "../../firebase/functions";
+import { useSelector } from "react-redux";
+import IsLoading from "../../components/loading/IsLoading";
+import { formatDate } from "../../helpers/Helpers";
+
+const mapState = ({ user }) => ({
+	userData: user.userData,
+	currentUser: user.currentUser,
+});
 
 const Chat = () => {
 	const history = useHistory();
-	const divRef = React.useRef();
-	let isUser = "jaya";
-	const messages = [
-		{ name: "jaya", message: "yoo" },
-		{ name: "john", message: "sup babe" },
-		{ name: "jaya", message: "yoo" },
-		{ name: "john", message: "alright" },
-		{ name: "jaya", message: "fine oh" },
-		{ name: "john", message: "lorem jatjakgiankmlkvijrykkjhushrui" },
-		{ name: "jaya", message: "yoo" },
-		{ name: "john", message: "sup babe" },
-		{ name: "jaya", message: "yoo" },
-		{ name: "john", message: "alright" },
-		{ name: "jaya", message: "fine oh" },
-		{ name: "john", message: "lorem jatjakgiankmlkvijrykkjhushrui" },
-	];
+	const [chats, setChats] = useState([]);
+	const [usrData, setUsrData] = useState({});
+	const [loading, setLoading] = useState(true);
+	const [message, setMessage] = useState("");
+	const divRef = useRef();
+	const { userData, currentUser } = useSelector(mapState);
+	const { userId, displayName, profilePic } = userData;
+	const { uid } = currentUser;
+	const d = userId || uid;
+	const { userChatId } = useParams();
+
 	const scrollInToView = () =>
 		divRef.current?.scrollIntoView({ behavior: "smooth" });
-	React.useEffect(() => {
+
+	useEffect(() => {
+		DB.collection("messages")
+			.doc(userChatId)
+			.collection("chat")
+			.orderBy("createdDate", "asc")
+			.onSnapshot((snapshot) => {
+				setChats(
+					snapshot.docs.map((doc) => ({
+						chatID: doc.id,
+						...doc.data(),
+					}))
+				);
+				setLoading(false);
+			});
+
 		scrollInToView();
 		return () => scrollInToView();
+	}, [userChatId]);
+
+	useEffect(() => {
+		DB.collection("users")
+			.doc(userChatId)
+			.get()
+			.then((snapshot) => {
+				if (snapshot.exists) {
+					setUsrData(snapshot.data());
+				}
+			})
+			.catch((err) => console.error(err));
 	}, []);
 	return (
 		<div className="chat-container">
@@ -39,14 +70,13 @@ const Chat = () => {
 						</div>
 						<div className="user-info">
 							<div className="user-info-img">
-								<Avatar
-									src="https://www.w3schools.com/howto/img_avatar.png"
-									alt=""
-								/>
+								<Avatar src={usrData?.profilePic} alt="" />
 							</div>
 							<div className="user-info-details">
-								<h5>Dprincecoder</h5>
-								<p>Online</p>
+								<Link to={`/users/user/${usrData?.userId}/profile`}>
+									{usrData?.displayName}
+								</Link>
+								<p>{usrData?.activeStatus}</p>
 							</div>
 						</div>
 					</div>
@@ -59,23 +89,47 @@ const Chat = () => {
 				<div className="divider"></div>
 				<div className="chat-body">
 					<div className={`chat-body-center`}>
-						{messages.map((i) => (
-							<>
-								<div
-									className={`${isUser === i.name ? "my-chat" : "other-chat"}`}>
-									{<p>{i.message}</p>}
+						{chats.length > 0 && !loading ? (
+							chats.map((i) => (
+								<div className="chat-hod">
+									{i.userThatSentChatPic !== profilePic && (
+										<div className="chat-img">
+											<Avatar src={i.userThatSentChatPic} />
+										</div>
+									)}
+									<div
+										className={`${
+											displayName === i.userThatSentChat
+												? "my-chat"
+												: "other-chat"
+										}`}>
+										{<p>{i.message}</p>}
+									</div>
+									<div className="time-date">{formatDate(i.createdDate)}</div>
 								</div>
-								<div className="time-date">2 hours ago</div>
+							))
+						) : (
+							<>
+								{loading ? (
+									<div className="lo">
+										<IsLoading />
+									</div>
+								) : (
+									<p className="no-msg">No Messages</p>
+								)}{" "}
 							</>
-						))}
+						)}
 					</div>
-					<div className="chat-body-bottom">
-						<InputForm type="text" placeholder="Type..." />
-						<Button type="submit" className="submit-btn">
+					<form className="chat-body-bottom">
+						<textarea
+							type="text"
+							placeholder="Chat..."
+							className="chat-body-text"></textarea>
+						<Button disabled={!message} type="submit" className="submit-btn">
 							<i className="material-icons">send</i>
 							<b style={{ dislay: "none" }} ref={divRef} />
 						</Button>
-					</div>
+					</form>
 				</div>
 			</div>
 		</div>
